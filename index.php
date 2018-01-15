fdaeb79591da45640a588fcfe865c4d8331d6ee2
miamor <miamorwest@gmail.com>
2018-01-15 10:06:42 +0700

    15.01.18

--- index.php
+++ index.php
@@ -5,14 +5,15 @@ include_once 'include/config.php';
 if (!$config->u) {
 	$page = 'login';
 } else {
-	if (!isset($page) || !$page) $page = 'trip';
+	if (!isset($page) || !$page) $page = 'index';
 
-	if (!file_exists('pages/'.$page.'.php')) $page = 'error';
+	if ($page != 'index' && !file_exists('pages/'.$page.'.php')) $page = 'error';
 }
 
 $page_title = 'AdminCP';
 
 if (!$do && !$v && !$temp) include 'header.php';
+if ($page == 'index') echo '<script>location.href = "'.MAIN_URL.'/trip";</script>';
 if ($page) {
 	include 'pages/'.$page.'.php';
 }
